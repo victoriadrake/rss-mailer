80c894a3dcc2cf98df7c70ff74f9179eb6541104
Victoria Drake <hello@victoria.dev>
2020-12-13 09:46:47 -0500

    Update helper script
    - Add template file to zip archive
    
    Close #1

--- scripts/update-lambda.sh
+++ scripts/update-lambda.sh
@@ -5,13 +5,13 @@ source .env
 GOOS=linux
 BUILD_NAME=${FUNCTION_NAME:-"rss-mailer"}
 
-go build && zip "$BUILD_NAME".zip "rss-mailer"
+go build && zip "$BUILD_NAME".zip rss-mailer template.html
 
 aws lambda update-function-code \
     --function-name "$BUILD_NAME" \
     --zip-file fileb://"$BUILD_NAME".zip
 
-rm "rss-mailer" "$BUILD_NAME".zip
+rm rss-mailer "$BUILD_NAME".zip
 
 aws lambda update-function-configuration \
     --function-name "$BUILD_NAME" \
